a7d612859f1e19bedd2266d7a796bf07d9fe9c25
Manojnahak7 <159554819+Manojnahak7@users.noreply.github.com>
2025-12-30 00:41:51 +0530

    Update AnalyticsController.java

--- backend/src/main/java/com/example/LandmineSoft/controller/AnalyticsController.java
+++ backend/src/main/java/com/example/LandmineSoft/controller/AnalyticsController.java
@@ -1,7 +1,7 @@
 package com.example.LandmineSoft.controller;
-import com.yourcompany.dto.PageVisitDto;
-import com.yourcompany.entity.PageVisit;
-import com.yourcompany.repository.PageVisitRepository;
+package com.example.LandmineSoft.dto.PageVisitDto;
+import com.example.LandmineSoft.entity.PageVisit;
+import com.example.LandmineSoft.repository.PageVisitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
