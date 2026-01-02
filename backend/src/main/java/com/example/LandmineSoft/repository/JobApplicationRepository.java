9b1e4df71d37fa8f7139197fc8cd62b087512f4b
Manojnahak7 <159554819+Manojnahak7@users.noreply.github.com>
2026-01-03 02:05:12 +0530

    Update JobApplicationRepository.java

--- backend/src/main/java/com/example/LandmineSoft/repository/JobApplicationRepository.java
+++ backend/src/main/java/com/example/LandmineSoft/repository/JobApplicationRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.example.LandmineSoft.entity.HiredApplication;
+
 import java.util.List;
 
 @Repository
