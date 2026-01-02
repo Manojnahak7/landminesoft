b114da1226704efc779ab0a69aefeb2313106a87
Manojnahak7 <159554819+Manojnahak7@users.noreply.github.com>
2026-01-03 02:08:02 +0530

    Update HiredApplicationRepository.java

--- backend/src/main/java/com/example/LandmineSoft/repository/HiredApplicationRepository.java
+++ backend/src/main/java/com/example/LandmineSoft/repository/HiredApplicationRepository.java
@@ -4,6 +4,8 @@ import com.example.LandmineSoft.entity.HiredApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List; 
+
 @Repository
 public interface HiredApplicationRepository extends JpaRepository<HiredApplication, Long> {
   List<HiredApplication> findByEmail(String email);
